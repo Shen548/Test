e3fc8420f2171e4b8110dee71e1cd0bd77321354
Shen548 <shen548@gmail.com>
2018-01-25 15:43:44 -0700

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,6 +10,8 @@ class Portfolio < ApplicationRecord
 
   after_initialize :set_defaults
 
+  has_many :technologies
+
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height:'600', width:'400')
     self.thumb_image ||= Placeholder.image_generator(height:'350', width:'200')
